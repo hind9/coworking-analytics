9fdd859e38747cdd061c9737582322070c16f1d9
Hind Almutairi <hind9.1437@gmail.com>
2025-09-06 22:38:32 +0300

    Update app.py

--- analytics/app.py
+++ analytics/app.py
@@ -35,7 +35,7 @@ def health_check():
 @app.route("/readiness_check")
 def readiness_check():
     try:
-        count = db.session.execute(text("SELECT COUNT(*) FROM tokens")).scalar()
+        count = db.session.execute(text("SELECT 1")).scalar()
     except Exception as e:
         app.logger.error(e)
         return "failed", 500
