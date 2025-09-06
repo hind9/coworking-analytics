d83ac9e6fcb22bc28100db8dfd4e240523bace15
Hind Almutairi <hind9.1437@gmail.com>
2025-09-07 02:36:50 +0300

    Update config.py

--- analytics/config.py
+++ analytics/config.py
@@ -1,11 +1,11 @@
 import os
 
-DB_USERNAME = os.environ.get("DB_USERNAME")
+DB_USER = os.environ.get("DB_USER")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
 DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME")
 
 SQLALCHEMY_DATABASE_URI = (
-    f"postgresql+psycopg://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
